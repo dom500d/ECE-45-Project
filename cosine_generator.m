1d39d3529add14fc06c12a4644134d84d91ad2ca
kungh <kungh@100.117.59.32>
2022-03-08 19:58:48 -0800

    added sine and cosine generators

---
+++ cosine_generator.m
@@ -0,0 +1,14 @@
+% Author: Harrison Kung
+% Usage: Takes in 5 variables for amplitude, phase, frequency, sampling
+%        frequency, duration
+function output = cosine_generator(amp, phase, freq, samfreq, duration)
+    % set output array to empty array with total # of samples (duration in
+    % seconds multiplied by the sample frequency)
+    output = zeros(1, duration * samfreq);
+    % iterate through entire array
+    for i = 1 : (duration * samfreq)
+        % each element in array is 2 pi frequency i divided by the sample
+        % frequency shifted by the phase and multiplied by the amplitude
+        output(i) = amp * cos(((2*pi*freq*(i))/(samfreq))-phase);
+    end
+end
