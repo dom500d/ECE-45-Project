e83e93fbaa3b4d5f7f81946351952dc30227db74
holdena1 <holden.adamec@gmail.com>
2022-03-10 20:53:17 -0800

    Rewrote the adsr code myself. Now works with inputs of any duration

--- adsr_gen.m
+++ adsr_gen.m
@@ -1,31 +1,56 @@
-function a = adsr_gen(target,gain,fs,duration)
-% Author: Phillip L. De Leon
-% Source: https://www.mathworks.com/matlabcentral/answers/uploaded_files/223069/ASEE_GSW_2000.pdf
-% Input
-%   target   - vector of attack, sustain, release target values
-%   gain     - vector of attack, sustain, release gain values
-%   fs       - sampling frequency
-%   duration - vector of attack, sustain, release durations in ms
-% Output
-%   a        - vector of adsr envelope values
-a = zeros(fs,1);  % assume 1 second duration ADSR envelope
-duration = round(duration./1000.*fs); % envelope duration in samp
-% Attack phase
-start = 2;
-stop = duration(1);
-for n = [start:stop]
-   a(n) = target(1)*gain(1) + (1.0 - gain(1))*a(n-1);
+function output = adsr_gen(attack,sustain,length,fs,duration)
+% ADSR_GEN This creates an adsr envelope which we can modulate with an input signal
+% 
+% Author: Holden Adamec
+% Used this as a reference while making my code:
+% https://www.mathworks.com/matlabcentral/answers/uploaded_files/223069/ASEE_GSW_2000.pdf
+%
+% The graph will rise from 0 to the value of attack (attack), decrease to
+% sustain (decay), stay constant (sustain), then fall back to 0 (release)
+%
+%   attack - the value that will be reached by attack
+%   sustain - the value of the sustain segment
+%   length - a vector containing 3 percentages for the lengths of attack,
+%   decay, and sustain respectively. Whatever is left over will be used for
+%   release
+%   fs - sampling frequency (1/s)
+%   duration - the length of the signal (s)
+%
+% Usage:
+% adsr = adsr(attack,sustain,[...],fs,duration);
+% out = adsr(:) .* input(:); Modulate with an input signal
+samples = fs * duration;
+output = zeros(samples, 1); % dimensions should be inverse of the signal
+
+% Attack
+start = 1;
+stop = start + samples * length(1);
+m = attack / (stop - start);
+for i = start:stop
+    output(i) = m * i;
 end
-% Sustain phase
+
+% Decay
 start = stop + 1;
-stop = start + duration(2);
-for n = [start:stop]
-   a(n) = target(2)*gain(2) + (1.0 - gain(2))*a(n-1);
+stop = start + samples * length(2);
+m = (sustain - attack) / (stop - start);
+for i = start:stop
+    output(i) = attack + m * (i - start);
 end
-% Release phase
+
+% Sustain
 start = stop + 1;
-stop = fs;
-for n = [start:stop]
-   a(n) = target(3)*gain(3) + (1.0 - gain(3))*a(n-1);
+stop = start + samples * length(3);
+for i = start:stop
+    output(i) = sustain;
 end
+
+% Release
+start = stop + 1;
+stop = samples;
+m = -sustain / (stop - start);
+for i = start:stop
+    output(i) = sustain + m * (i - start);
+end
+
 end
